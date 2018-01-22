799190e55ae2bbbcb1b42833aec42c2c07d4084b
Daniel Ménard <daniel.menard@laposte.net>
2018-01-22 15:38:09 +0100

    Config des tests unitaires

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is part of Docalist Postal Address Metadata.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\PostalAddressMetadata\Tests;
+
+require __DIR__ . '/../class/PostalAddressMetadata.php';
